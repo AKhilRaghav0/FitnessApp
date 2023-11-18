2f199676c172b74eb60192abe79295735db7fc40
Akhil Raghav <86706467+AKhilRaghav0@users.noreply.github.com>
2023-11-18 13:57:38 +0530

    CircleProgess VIew OPtimized

--- FitnessApp/Views/CircleProgress.swift
+++ FitnessApp/Views/CircleProgress.swift
@@ -10,89 +10,127 @@ import SwiftUI
 struct CircleProgress: View {
     @State var progress: Double = 3.4
     var body: some View {
-        HStack {
+        ScrollView(.horizontal, showsIndicators: false){
             HStack {
-                ZStack{
-                    
-                    Circle()
-                        .stroke(lineWidth: 20.0)
-                        .opacity(0.3)
-                        .foregroundColor(Color.gray)
-                    
-                    Circle()
-                        .trim(from: 0.0, to: CGFloat(min(self.progress, 0.3)))
-                        .stroke(style: StrokeStyle(lineWidth: 10.0, lineCap: .round, lineJoin: .round))
-                        .foregroundColor(Color.blue)
-                        .rotationEffect(Angle(degrees: 270.0)) // Start from the top
-                    
-    //                Text(String(format: "%.0f%%", min(self.progress, 1.0) * 100.0))
-    //                    .font(.title)
-    //                .fontWeight(.bold)
-                    
-                }
-                .frame(maxWidth: 50, maxHeight: 45)
-            .padding()
-                
-                VStack(alignment: .leading){
-                    Text("PIA")
-                        .fontWeight(.semibold)
-                    Image(systemName: "figure.run")
-                }
-                
-            }
-            .padding(.horizontal, 10)
-            .background(Color.gray.opacity(0.3))
-            .cornerRadius(10)
-            
-            
-            
-            
-            
-            
-            ZStack {
                 HStack {
                     ZStack{
                         
                         Circle()
                             .stroke(lineWidth: 20.0)
                             .opacity(0.3)
-                            .foregroundColor(Color.red.opacity(0.5))
+                            .foregroundColor(Color.gray)
                         
                         Circle()
-                            .trim(from: 0.0, to: CGFloat(min(self.progress, 0.9)))
+                            .trim(from: 0.0, to: CGFloat(min(self.progress, 0.3)))
                             .stroke(style: StrokeStyle(lineWidth: 10.0, lineCap: .round, lineJoin: .round))
-                            .foregroundColor(Color.red)
+                            .foregroundColor(Color.blue)
                             .rotationEffect(Angle(degrees: 270.0)) // Start from the top
                         
-        //                Text(String(format: "%.0f%%", min(self.progress, 1.0) * 100.0))
-        //                    .font(.title)
-        //                .fontWeight(.bold)
+                        //                Text(String(format: "%.0f%%", min(self.progress, 1.0) * 100.0))
+                        //                    .font(.title)
+                        //                .fontWeight(.bold)
                         
                     }
                     .frame(maxWidth: 50, maxHeight: 45)
-                .padding()
+                    .padding()
                     
                     VStack(alignment: .leading){
-                        Text("SPO").fontWeight(.semibold) + Text("2").font(.system(size: 12))
-                            
-                        Image(systemName: "heart.text.square")
+                        Text("PIA")
+                            .fontWeight(.semibold)
+                        Image(systemName: "figure.run")
                     }
                     
                 }
                 .padding(.horizontal, 10)
-                .background(Color.red.opacity(0.3))
+                .background(Color.gray.opacity(0.3))
                 .cornerRadius(10)
                 
+                
+                
+                
+                
+                
+                ZStack {
+                    HStack {
+                        ZStack{
+                            
+                            Circle()
+                                .stroke(lineWidth: 20.0)
+                                .opacity(0.3)
+                                .foregroundColor(Color.red.opacity(0.5))
+                            
+                            Circle()
+                                .trim(from: 0.0, to: CGFloat(min(self.progress, 0.9)))
+                                .stroke(style: StrokeStyle(lineWidth: 10.0, lineCap: .round, lineJoin: .round))
+                                .foregroundColor(Color.red)
+                                .rotationEffect(Angle(degrees: 270.0)) // Start from the top
+                            
+                            //                Text(String(format: "%.0f%%", min(self.progress, 1.0) * 100.0))
+                            //                    .font(.title)
+                            //                .fontWeight(.bold)
+                            
+                        }
+                        .frame(maxWidth: 50, maxHeight: 45)
+                        .padding()
+                        
+                        VStack(alignment: .leading){
+                            Text("SPO").fontWeight(.semibold) + Text("2").font(.system(size: 12))
+                            
+                            Image(systemName: "heart.text.square")
+                        }
+                        
+                    }
+                    .padding(.horizontal, 10)
+                    .background(Color.red.opacity(0.3))
+                    .cornerRadius(10)
+                    
+                }
+                
+                ZStack {
+                    HStack {
+                        ZStack{
+                            
+                            Circle()
+                                .stroke(lineWidth: 20.0)
+                                .opacity(0.3)
+                                .foregroundColor(Color.cyan.opacity(0.5))
+                            
+                            Circle()
+                                .trim(from: 0.0, to: CGFloat(min(self.progress, 0.2)))
+                                .stroke(style: StrokeStyle(lineWidth: 10.0, lineCap: .round, lineJoin: .round))
+                                .foregroundColor(Color.cyan)
+                                .rotationEffect(Angle(degrees: 270.0)) // Start from the top
+                            
+                            //                Text(String(format: "%.0f%%", min(self.progress, 1.0) * 100.0))
+                            //                    .font(.title)
+                            //                .fontWeight(.bold)
+                            
+                        }
+                        .frame(maxWidth: 50, maxHeight: 45)
+                        .padding()
+                        
+                        VStack(alignment: .leading){
+                            Text("Stress").fontWeight(.semibold)
+                            
+                            Image(systemName: "dumbbell")
+                        }
+                        
+                    }
+                    .padding(.horizontal, 10)
+                    .background(Color.cyan.opacity(0.3))
+                    .cornerRadius(10)
+                    
+                }
             }
-            
-            
         }
+        .padding()
         
-        
-        
-        
-            
     }
+    
+    
+    
+    
+    
 }
 
 struct CircleProgress_Previews: PreviewProvider {
