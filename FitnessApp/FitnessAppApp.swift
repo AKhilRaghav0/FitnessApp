5e1d6cc0c9d405970e7c460942adee3848617c45
Akhil Raghav <86706467+AKhilRaghav0@users.noreply.github.com>
2023-11-18 10:46:06 +0530

    Initial Commit

---
+++ FitnessApp/FitnessAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  FitnessAppApp.swift
+//  FitnessApp
+//
+//  Created by Akhil on 18/11/23.
+//
+
+import SwiftUI
+
+@main
+struct FitnessAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
