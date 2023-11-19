9902cce423c6817426983bef47cb1a2ced447abb
Akhil Raghav <86706467+AKhilRaghav0@users.noreply.github.com>
2023-11-19 21:02:43 +0530

    Tab View Added

--- FitnessApp/ContentView.swift
+++ FitnessApp/ContentView.swift
@@ -1,13 +1,29 @@
-//
-//  ContentView.swift
-//  FitnessApp
-//
-//  Created by Akhil on 18/11/23.
-//
-
 import SwiftUI
 
 struct ContentView: View {
+    var body: some View {
+        TabView {
+            ContentViewContent()
+                .tabItem {
+                    Label("Home", systemImage: "house")
+                }
+            
+            Text("Second Tab Content")
+                .tabItem {
+                    Label("Tab 2", systemImage: "2.circle")
+                }
+            
+            WaterCounter()
+                .tabItem {
+                    Label("Tab 3", systemImage: "drop.fill")
+                }
+                .blur(radius: 55)
+                .background(.thickMaterial.opacity(0.3))
+        }
+    }
+}
+
+struct ContentViewContent: View {
     var body: some View {
         ScrollView {
             VStack(alignment: .leading, spacing: 10) {
@@ -25,9 +41,14 @@ struct ContentView: View {
     }
 }
 
-
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
+    }
+}
+
+struct SecondTabContent: View {
+    var body: some View {
+        Text("Second Tab Content")
     }
 }
