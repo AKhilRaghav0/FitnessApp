632ff96911a618383ba3c24d4aed196ad70c0108
Akhil Raghav <86706467+AKhilRaghav0@users.noreply.github.com>
2023-11-21 15:06:13 +0530

    Icons removed from Account linking

--- FitnessApp/Views/ViewModel/accountLinkingPage.swift
+++ FitnessApp/Views/ViewModel/accountLinkingPage.swift
@@ -13,50 +13,50 @@ struct accountLinkingPage: View {
             VStack {
                 List {
                     HStack {
-                        Image("google-fit")
-                            .resizable()
-                            .scaledToFit()
-                            .frame(width: 30, height: 40)
+//                        Image("google-fit")
+//                            .resizable()
+//                            .scaledToFit()
+//                            .frame(width: 30, height: 40)
                         NavigationLink("Google Fit", destination: Text("To google Fit  page soon"))
                     }
                     HStack {
-                        Image("Strava_logo")
-                            .resizable()
-                            .scaledToFit()
-                            .frame(width: 30, height: 40)
+//                        Image("Strava_logo")
+//                            .resizable()
+//                            .scaledToFit()
+//                            .frame(width: 30, height: 40)
                         NavigationLink("Strava", destination: Text("To Strava Page soon"))
                     }
                     HStack {
-                        Image("relive")
-                            .resizable()
-                            .scaledToFit()
-                            .clipShape(Circle())
-                            .frame(width: 30, height: 30)
+//                        Image("relive")
+//                            .resizable()
+//                            .scaledToFit()
+//                            .clipShape(Circle())
+//                            .frame(width: 30, height: 30)
                         NavigationLink("Relive", destination: Text("To Relive Auth Page"))
                     }
                     HStack {
-                        Image("alexa")
-                            .resizable()
-                            .scaledToFit()
-                            .clipShape(Circle())
-                            .frame(width: 30, height: 30)
+//                        Image("alexa")
+//                            .resizable()
+//                            .scaledToFit()
+//                            .clipShape(Circle())
+//                            .frame(width: 30, height: 30)
                         NavigationLink("Amazon Alexa", destination: Text("To Amazon Alexa Auth Page"))
                         
                     }
                     HStack {
-                        Image("adidas_logo")
-                            .resizable()
-                            .scaledToFit()
-                            .clipShape(Circle())
-                            .frame(width: 30, height: 30)
+//                        Image("adidas_logo")
+//                            .resizable()
+//                            .scaledToFit()
+//                            .clipShape(Circle())
+//                            .frame(width: 30, height: 30)
                         NavigationLink("addidas Running", destination: Text("To addidas running web page"))
                     }
                     HStack {
-                        Image("Komoot_logo")
-                            .resizable()
-                            .scaledToFit()
-                            .clipShape(Circle())
-                            .frame(width: 30, height: 30)
+//                        Image("Komoot_logo")
+//                            .resizable()
+//                            .scaledToFit()
+//                            .clipShape(Circle())
+//                            .frame(width: 30, height: 30)
                         NavigationLink("Komoot", destination: Text("To komoot Destination Page"))
                     }
                 }
